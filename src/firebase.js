c350e1bc1467e2c35b159cbacea106f056e87d43
GustavM01 <gustav.milihagulu@gmail.com>
2025-07-08 16:12:58 +0200

    Removed API key from public file

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZGOGj261bw4rcKxlXQxnRfmYnwBK1wig",
-  authDomain: "react-blog-page-58678.firebaseapp.com",
-  projectId: "react-blog-page-58678",
-  storageBucket: "react-blog-page-58678.firebasestorage.app",
-  messagingSenderId: "1098077244333",
-  appId: "1:1098077244333:web:3e6f4504322c984b896f24",
-  measurementId: "G-732NRGEXQS",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
